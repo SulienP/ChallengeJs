18ccfba3340536a25db2f5b7da2a3612a1805956
SulienP <112190974+SulienP@users.noreply.github.com>
2023-05-11 12:14:34 +0200

    ajout du fight

--- yofclan/src/element/figthGestion/fight.js
+++ yofclan/src/element/figthGestion/fight.js
@@ -8,7 +8,45 @@ function fight(){
         attackValue = attackValue + element.attaque;
         defenseValue = defenseValue + element.defense;
     }
-    console.log(attackValue, defenseValue)
+    const pourcentOfDefeat = Math.random() * (attackValue - (attackValue - 1) + 1) + attackValue - 1;
+    if (pourcentOfDefeat > attackValue - 0.1) {
+        
+        const defense = (Math.random() * (defenseValue - (defenseValue - 1) + 1) + defenseValue - 1).toFixed(1);
+        const pourcentOfDeath = defenseValue - defense
+        if (pourcentOfDeath <= 0) {
+            alertWindosErro("nobody")
+        } else if (pourcentOfDeath >= 0.5 &&  pourcentOfDeath <= 0.9) {
+            for (let i = 0; i < (myMairie.stockageOfFigthers.length /2); i++) {
+                myMairie.stockageOfFigthers = myMairie.stockageOfFigthers[i]
+            }
+            alertWindosErro("divideByTwo");
+        } else if (myMairie.stockageOfFigthers.length >= 0.9) {
+            myMairie.stockageOfFigthers = [];
+            alertWindosErro("everyBody")
+        }
+    } else {
+        myMairie.currentOil = myMairie.currentOil + (100 * myMairie.level)
+        myMairie.currentGold = myMairie.currentGold + (100 * myMairie.level)
+        alertWindosErro("win")
+    }
+    function alertWindosErro(element, value) {
+        switch (element) {
+            case "nobody":
+                window.alert("You loose but nobody death")
+                break;
+            case "divideByTwo":
+                window.alert("You loose your fight and you army is divide by twoo")
+                break;
+            case "everyBody":
+                window.alert("You loose and everybody is dead");
+                break
+            case "win":
+                window.alert("Congratulations you win and nobdy is dead you win " + myMairie.currentOil + (100 * myMairie.level) + " oil and " + myMairie.currentGold + (100 * myMairie.level), +" gold ");
+                break;
+            default:
+                break;
+        }
+    }
 }
 
 export default fight;
